b4f7c61b4f8b29ecdc452bae48d131d6ca062627
Dhruvinsinh Rathod <dhruvinsinhrathod12@gmail.com>
2023-03-24 00:52:19 +0530

    DailyJava

--- src/StudentMarks.java
+++ src/StudentMarks.java
@@ -4,7 +4,7 @@ public class StudentMarks {
         System.out.println("Enter the total number");
          Scanner sc= new Scanner(System.in);
          int total= sc.nextInt();
-         float b=0;
+         float b=0; 
          float per=0;
          for (int i=1;i<total;i++) {
         	 System.out.println("Enter "+ i+"th subject Mark:");
--- src/SubString.java
+++ src/SubString.java
@@ -35,7 +35,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class SubString {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
